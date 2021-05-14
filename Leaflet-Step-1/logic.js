658116cc74ecfb0578898e1354384e23d1800fe1
j0serobles <engjoserobles@gmail.com>
2021-05-13 21:53:27 -0400

    Completed Step 1

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,6 +1,39 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// Get a value between 0 and 1 for marker opacity
+function getOpacity(depth) {
+ if (depth <= 10) {
+   return(0.05);
+ }
+ else if (depth <= 20) {
+   return (.20);
+ }
+ else if(depth <= 30) {
+   return (.30); 
+ }
+ else if(depth <= 40) {
+  return (.40); 
+}
+else if(depth <= 50) {
+  return (.50); 
+}
+else if(depth <= 60) {
+  return (.60); 
+}
+else if(depth <= 70) {
+  return (.70); 
+}
+else if(depth < 80) {
+  return (.80); 
+}
+else if(depth < 90) {
+  return (.90); 
+}
+else if(depth <= 100) {
+  return (.95); 
+}
+}
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
@@ -14,13 +47,27 @@ function createFeatures(earthquakeData) {
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + 
+      "<p>Magnitude:" + feature.properties.mag +", Depth (km):" + feature.geometry.coordinates[2] +"</p>"
+      );
   }
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature, 
+
+    pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, {
+				radius: feature.properties.mag * 6,
+				fillColor: "#ff7800",
+				color: "#000",
+				weight: 1,
+				opacity: 1,
+				fillOpacity: getOpacity(feature.geometry.coordinates[2])
+			});
+		}
+
   });
 
   // Sending our earthquakes layer to the createMap function
