dd96ee9dc88b4dc6bc2f728e1eb7a8f5c9a1a690
j0serobles <engjoserobles@gmail.com>
2021-05-15 10:45:19 -0400

    Completed Step 2

--- Leaflet-Step-2/logic.js
+++ Leaflet-Step-2/logic.js
@@ -4,36 +4,19 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Get a value between 0 and 1 for marker opacity
 function getOpacity(depth) {
- if (depth <= 10) {
-   return(0.05);
- }
- else if (depth <= 20) {
-   return (.20);
- }
- else if(depth <= 30) {
-   return (.30); 
- }
- else if(depth <= 40) {
-  return (.40); 
-}
-else if(depth <= 50) {
-  return (.50); 
-}
-else if(depth <= 60) {
-  return (.60); 
-}
-else if(depth <= 70) {
-  return (.70); 
-}
-else if(depth < 80) {
-  return (.80); 
-}
-else if(depth < 90) {
-  return (.90); 
-}
-else if(depth <= 100) {
-  return (.95); 
-}
+    if (depth <= 70) {
+      return(0.1);
+    }
+    else if (depth <= 300) {
+      return (.5);
+    }
+    else if(depth <= 700) {
+      return (.9); 
+    }
+    else {
+      return (1); 
+    }
+
 }
 // Perform a GET request to the query URLs
 d3.json(queryUrl)
@@ -104,10 +87,19 @@ function createMap(earthquakes, boundaries) {
     accessToken: API_KEY
   });
 
+  var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "satellite-v9",
+    accessToken: API_KEY
+  });
+  
+
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
-    "Dark Map": darkmap
+    "Dark Map": darkmap,
+    "Satelite" : satellite
   };
 
   // Create overlay object to hold our overlay layer
@@ -131,4 +123,20 @@ function createMap(earthquakes, boundaries) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+  // Set up the legend
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+ var div = L.DomUtil.create("div", "info legend"); 
+  // Add min & max
+  var legendInfo  = '<h3 style="text-align:center">Depth (KM)</h3>';
+      legendInfo += '<i style="background: #ff7800; opacity:.1;"></i><span>0-70 (Shallow)</span><br>';
+      legendInfo += '<i style="background: #ff7800; opacity:.5;"></i><span>70-300 (Intermed)</span><br>';
+      legendInfo += '<i style="background: #ff7800; opacity:.9;"></i><span>300-700 (Deep)</span><br>';
+  div.innerHTML   = legendInfo;
+  return div;
+};
+
+  // Adding legend to the map
+  legend.addTo(myMap);
+
 }
